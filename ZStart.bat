afdc68d9c9d66b06b8de19eb4e790bbff66bf8d6
ConaII <72966048+ConaII@users.noreply.github.com>
2021-09-15 19:42:16 -0300

    Windows Start Script - v1.4.4

--- ZStart.bat
+++ ZStart.bat
@@ -12,7 +12,7 @@ title ZStart
 
 ::--CONSOLE--::
 ::# Sets the console title.
-SET TITLE="Minecraft: Server Instance"
+SET TITLE="UT:Re-Birth: Build+"
 ::# Sets the server location.
 SET SERVER_PATH="./"
 ::# Loop the server, no matter how the server closes, it will restart.
@@ -34,16 +34,19 @@ SET EXIT=true
 ::# The name of your server's JAR file.
 ::# Side note: if you're not using Paper (http://papermc.io) or one of it's forks,
 ::# (https://purpur.pl3x.net) then you should really switch. 
-SET JAR_NAME="server.jar"
+SET JAR_NAME="purpur-1.16.5-1171.jar"
 ::# The location of your server's JAR.
 SET JAR_PATH="./"
+::# Sets the CPU priority for the java program (Server).
+::# Values: REALTIME | LOW, BELOWNORMAL, NORMAL, ABOVENORMAL, HIGH
+SET PRIORITY=REALTIME
 ::# Skips the need of eula.txt = true.
 SET EULA-SKIP=false
 ::# Removes warnings about outdated server builds.
 SET ADVANCED-MODE=true
 ::# Your server's JAR parameters.
-::# In order to see all of the parameters, set below --help.
-::# (This wont let the server start, but will show all parameters aviable)
+::# In order to see all the parameters, set below --help.
+::# (This won't let the server start, but will show all parameters available)
 ::# More info:
 ::#   https://minecraft.fandom.com/wiki/Tutorials/Setting_up_a_server
 ::#   https://bukkit.gamepedia.com/CraftBukkit_Command_Line_Arguments
@@ -56,7 +59,7 @@ SET PARAMETERS="--nogui"
 ::# You can use this to determine which java you want to use.
 ::# Consider using java 11 or latest if possible, and changing to OpenJDK.
 ::# (Change to OpenJDK here! https://adoptopenjdk.net/)
-SET JAVA_BINARY="java"
+SET JAVA_BINARY="C:\Program Files\AdoptOpenJDK\jdk-16.0.1.9-hotspot\bin\java.exe"
 ::# Allocate the server's max&min heap-memory.
 ::# If you want to allocate more than 4GB you will need Java 64 bits.
 SET MAX-MEMORY=4096
@@ -64,12 +67,12 @@ SET MIN-MEMORY=1024
 ::# Select the memory unit to be used.
 ::# Values: KB, MB, GB.
 SET HEAP_UNIT=MB
-::# This attemps to self-calculate the nursery max&min memory based on max-heap-memory.
+::# This attempts to self-calculate the nursery max&min memory based on max-heap-memory.
 ::# Java already sets this itself. OpenJ9 is needed.
 SET NURSERY=false
 
 ::--FLAGS--::
-::# DEFAULTS: Always enabled, independant of other group flags.
+::# DEFAULTS: Always enabled, independently of other group flags.
 ::# G1GC: Aikar's G1GC flags. (https://mcflags.emc.gs)
 ::# ZGC: Aikar based ZGC flags. (Exists from J11, decent from J14, Latest is always better.)
 
@@ -129,11 +132,14 @@ SET Start_SERVICE=NONE
 ::other
 set TITLE=%TITLE:~1,-1%
 set JAR_PATH=%JAR_PATH:~1,-1%
+set JAR_NAME=%JAR_NAME:~1,-1%
 set DEFAULTS=%DEFAULTS:~1,-1%
 set MODE=%HEAP_UNIT:~0,1%
-::console-server
+::setup
 title %TITLE%
 cd %SERVER_PATH%
+echo %DATE% - %TIME%
+::console-server
 if /I '%AUTO-EULA%'=='true' (
 	@echo eula=true>"%JAR_PATH%eula.txt"
 )
@@ -141,32 +147,40 @@ if /I '%1'=='-restart' GOTO restart
 if /I '%EULA-SKIP%'=='true' set agree=-DCom.mojang.eula.agree=true
 if /I '%ADVANCED-MODE%'=='true' set advanced=-DIReallyKnowWhatIAmDoingISwear=true
 ::TASKS&SERVICES 
-if %Start_TASK%'=='NONE' SCHTASKS /DELETE /TN %ServiceName%
-if %Start_SERVICE%'=='NONE' sc delete %ServiceName%
-if NOT '%Start_TASK%'=='NONE' if '%Start_SERVICE%'=='NONE' SCHTASKS /CREATE /TN %ServiceName% /SC %Start_TASK% /ST %TaskTime% /TR %0 
-if NOT '%Start_SERVICE%'=='NONE' if '%Start_TASK%'=='NONE' sc create '%ServiceName%' start=%Start_SERVICE% binpath=%0
+schTasks /QUERY /TN %ServiceName% >NUL 2>&1
+if 'ERRORLEVEL'=='0' schTasks /DELETE /TN %ServiceName% 
+
+sc QUERY %ServiceName% >NUL 2>&1
+if 'ERRORLEVEL'=='0 sc delete %ServiceName%
+if NOT '%Start_TASK%'=='NONE' if '%Start_SERVICE%'=='NONE' ( 
+	echo.
+	schTasks /CREATE /TN %ServiceName% /SC %Start_TASK% /ST %TaskTime% /TR %0
+)
+if NOT '%Start_SERVICE%'=='NONE' if '%Start_TASK%'=='NONE' (
+	echo.
+	sc create '%ServiceName%' start=%Start_SERVICE% binpath=%0
+)
 ::FLAGS
 setlocal ENABLEDELAYEDEXPANSION
 set "JVM_FLAGS=!%FLAGS_MODE%! %JVM_MF% %DEFAULTS% %agree% %advanced%"
 if /I '%MULTI_FLAGS%'=='true' set JVM_MF=!%FLAGS_MODE1%! !%FLAGS_MODE2%! !%FLAGS_MODE3%! !%FLAGS_MODE4%! !%FLAGS_MODE5%!
-FOR /F delims^=^"^ tokens^=* %%a in (%JVM_FLAGS%) do set JVM_FLAGS=%%a
-FOR /F delims^=^"^ tokens^=* %%a in (%JVM_MF%) do set JVM_MF=%%a
 setlocal DISABLEDELAYEDEXPANSION
 )
 :: NURSERY
 if '%NURSERY%'=='TRUE' (
 	set /A NURSE_MAX=%MAX-MEMORY% * 2 / 5
 	set /A NURSE_MIN=%MAX-MEMORY% / 4
-	set CMD=%JAVA_BINARY% -Xmx%MAX-MEMORY%%MODE% -Xms%MIN-MEMORY%%MODE% -Xmns%NURSE_MIN%%MODE% -Xmnx%NURSE_MAX%%MODE% %JVM_FLAGS% %12flags% "%JAR_PATH%%JAR_NAME%" %PARAMETERS%
-) else set CMD=%JAVA_BINARY% -Xmx%MAX-MEMORY%%MODE% -Xms%MIN-MEMORY%%MODE% %JVM_FLAGS% %12flags% "%JAR_PATH%%JAR_NAME%" %PARAMETERS%
+	set CMD=%JAVA_BINARY% -Xmx%MAX-MEMORY%%MODE% -Xms%MIN-MEMORY%%MODE% -Xmns%NURSE_MIN%%MODE% -Xmnx%NURSE_MAX%%MODE% %JVM_FLAGS% -jar "%JAR_PATH%%JAR_NAME%" %PARAMETERS%
+) else set CMD=%JAVA_BINARY% -Xmx%MAX-MEMORY%%MODE% -Xms%MIN-MEMORY%%MODE% %JVM_FLAGS% -jar "%JAR_PATH%%JAR_NAME%" %PARAMETERS%
 
 ::Start server
-echo %DATE% - %TIME% 
 echo.
 echo Starting Server 
 echo Loading startup parameters...
 echo.
-%CMD%
+start /I /B /W "%TITLE%" /%PRIORITY% %CMD%
+echo.
+echo.
 goto stop
 
 :stop
@@ -178,6 +192,9 @@ if /I '%RESTART_LOOP%'=='true' ( goto restart
 goto exit
 
 :exit
+echo.
+echo Exiting Server
+echo.
 if /I '%EXIT%'=='true' exit
 
 :restart
