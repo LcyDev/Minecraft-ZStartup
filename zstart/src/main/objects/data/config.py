86229c7f9c2988c633aac505b120bdd713d25737
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2024-05-01 00:21:27 -0300

    Add config JIT methods

--- zstart/src/main/objects/data/config.py
+++ zstart/src/main/objects/data/config.py
@@ -58,3 +58,46 @@ class ConfigData():
             defaults: str | list[str] = ""
             modules_path: str = "./flags"
             modules: str | list[str] = ""
+
+    def get_exec_path(self) -> str:
+        return self.server.jar_path + self.server.jar_name
+
+    def get_jar_flags(self) -> list[str]:
+        flags = []
+        self.add_auto_flags(flags)
+        flags.extend(useful.slith(self.server.before_jar))
+        return flags
+
+    def get_jar_args(self) -> list[str]:
+        args = []
+        args.extend(useful.slith(self.server.parameters))
+        return args
+
+    def get_jvm_flags(self) -> list[str]:
+        flags = []
+        flags.extend(useful.slith(self.java.flags.defaults))
+        # TODO: Add support for flag modules
+        return flags
+
+    def get_jvm_mem(self) -> list[str]:
+        xms = useful.get_percentage(self.java.min_memory, self.java.memory_percent)
+        xmx = useful.get_percentage(self.java.max_memory, self.java.memory_percent)
+
+        return [f'-Xms{xms}{self.java.heap_unit}', f'-Xmx{xmx}{self.java.heap_unit}']
+
+    def add_auto_flags(self, flags: list) -> None:
+        toggles = self.server.auto_flags
+        if toggles.log4j2_debug:
+            flags.append("-Dlog4j2.debug=TRUE")
+        if toggles.log4j2_configFile:
+            flags.append(f"-Dlog4j2.configurationFile={toggles.log4j2_configFile}")
+
+        if toggles.file_encoding:
+            flags.append(f"-Dfile.encoding={toggles.file_encoding}")
+
+        if toggles.eula_skip:
+            flags.append("-Dcom.mojang.eula.agree=TRUE")
+        if toggles.paper_log_level:
+            flags.append(f"-Dpaper.log-level={toggles.paper_log_level}")
+        if toggles.paper_skip_properties_comments:
+            flags.append("-Dpaper.skipServerPropertiesComments=TRUE")
