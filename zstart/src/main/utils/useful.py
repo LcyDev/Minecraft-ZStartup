e572eafba9d0a71fea3a3a78474d3b1aaaf442bf
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2024-05-02 18:29:02 -0300

    Pending modules.

--- zstart/src/main/utils/useful.py
+++ zstart/src/main/utils/useful.py
@@ -15,11 +15,3 @@ def slith(value: str | list[str]) -> list[str]:
 
 def get_current_time() -> str:
     return datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
-
-def get_modules(path: str, modules: str | list[str]):
-    modules = slith(modules)
-
-    for module in modules:
-        if not Path(path / module).exists():
-            print(f"Module {module} not found in {path}.")
-            continue
