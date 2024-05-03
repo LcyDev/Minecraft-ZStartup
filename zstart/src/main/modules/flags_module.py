f3bc4b40eaad63fdb01935acc063eaeb74dcb9ac
Lucyy_ <conahiromi@gmail.com>
2024-05-02 22:34:41 -0300

    Update flags_module.py

--- zstart/src/main/modules/flags_module.py
+++ zstart/src/main/modules/flags_module.py
@@ -1,9 +1,19 @@
-class FlagsModule:
+class FlagsGroup:
     name: str
     enabled: bool
     overrides: list
-    incompatible: list
-    flags: list
+    incompatible: list[str]
+    flags: list[str]
+    
+    def __init__(  ):
+        ...
+
+
+class FlagsModule:
+    name: str
+    enabled: bool
+    incompatible: list[str]
+    groups: list[FlagsGroup]
     
     def __init__( modulePath: str ):
         ...
